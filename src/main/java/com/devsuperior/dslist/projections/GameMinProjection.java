ff3a88f69a2ce0e961041c03b8d9974df37c02a3
Nathany <nathanyk.morais@gmail.com>
2023-11-15 16:24:25 -0300

    projection e get games by list

---
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dslist.projections;
+
+public interface GameMinProjection {
+	
+	//métodos Gets correspondentes a minha consulta no banco de dados (feita no GameRepository através)
+	
+	Long getId();
+	String getTitle();
+	Integer getYear();
+	String getImgUrl();
+	String getShortDescription();
+	Integer getPosition();
+	
+}
